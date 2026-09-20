21ef1429f6d62202ce804a849ac67aa434d7653e
کانتر مود <counter2mod.ir@gmail.com>
2026-09-20 23:12:03 +0330

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,230 @@
+/*
+ * Counter Mod
+ * Website Data
+ * Creator: ابوالفضل کاظمی
+ */
 
+const DEFAULT_DATA = {
+
+    site: {
+        title: "کانتر مود",
+        subtitle: "کانتر مود روبیکا",
+        creator: "ابوالفضل کاظمی",
+
+        mainImage:
+            "https://cdn.imgurl.ir/uploads/x012403_InShot_20260625_163753784.png",
+
+        aiUrl:
+            "https://holobot.ir/s/html/هوش%20مصنوعی%20آفلاین.html",
+
+        channel:
+            "https://rubika.ir/Counter2Mod",
+
+        creatorUrl:
+            "https://rubika.ir/Abolfazl_Kazemi_SFG",
+
+        telegram:
+            "https://t.me/Counter2Mod",
+
+        bale:
+            "https://ble.ir/Countermod",
+
+        aparat:
+            "https://www.aparat.com/Abolfazl_Hakerr/shorts",
+
+        website:
+            "https://B2n.ir/Counter2Mod",
+
+        group:
+            "https://rubika.ir/joing/BAFCGFBCE0SVOLBSWKUNPCLEJNAMITSH"
+    },
+
+    tutorial: {
+        aparat:
+            "https://www.aparat.com/video/video/embed/videohash/rlk4a91/vt/frame",
+
+        mp4:
+            "https://cdn.imgurl.ir/uploads/p041630_InShot_20260601_025037109.mp4"
+    },
+
+    downloads: [
+
+        {
+            id: 1,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/e746382_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGGHEFCCGGEEHABG"
+        },
+
+        {
+            id: 2,
+            title: "کانتر۲ مود منو FL",
+            image: "https://cdn.imgurl.ir/uploads/g317464_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGHHBEGBJCFFIABG"
+        },
+
+        {
+            id: 3,
+            title: "کانتر۲ مود منو دوتایی جدید",
+            image: "https://cdn.imgurl.ir/uploads/a959848_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIFJAHFDCFBEABG"
+        },
+
+        {
+            id: 4,
+            title: "کانتر۲ مود منو جدید",
+            image: "https://cdn.imgurl.ir/uploads/f81909_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIFJCIFEHDGHABG"
+        },
+
+        {
+            id: 5,
+            title: "کانتر۲ مود منو دوتایی بدون دیتا",
+            image: "https://cdn.imgurl.ir/uploads/b23474_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIGECDHJDIAEABG"
+        },
+
+        {
+            id: 6,
+            title: "کانتر۲ مود منو خفن",
+            image: "https://cdn.imgurl.ir/uploads/r5684_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIIDHGHIDCDBABG"
+        },
+
+        {
+            id: 7,
+            title: "کانتر۲ مود منو FL تکی",
+            image: "https://cdn.imgurl.ir/uploads/j57884_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIIDJEHJCJHJABG"
+        },
+
+        {
+            id: 8,
+            title: "مود منو بدون دیتا با قابلیت جدید",
+            image: "https://cdn.imgurl.ir/uploads/x476408_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIIEHBICCBJAABG"
+        },
+
+        {
+            id: 9,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/r961755_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGIIFIGIHBGBCABG"
+        },
+
+        {
+            id: 10,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/q598661_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJBDGGCHHDCAABG"
+        },
+
+        {
+            id: 11,
+            title: "کانتر۲ مود منو جدید",
+            image: "https://cdn.imgurl.ir/uploads/t56451_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJBDHICIBHHDABG"
+        },
+
+        {
+            id: 12,
+            title: "کانتر۲ مود منو بدون لگ",
+            image: "https://cdn.imgurl.ir/uploads/l71334_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJBEFBDBGFDHABG"
+        },
+
+        {
+            id: 13,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/h2234_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJBGHIECBFCGABG"
+        },
+
+        {
+            id: 14,
+            title: "کانتر۲ مود منو دیتا دار",
+            image: "https://cdn.imgurl.ir/uploads/i110244_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJEIHCADDDDHABG"
+        },
+
+        {
+            id: 15,
+            title: "کانتر۲ مود منو خفن",
+            image: "https://cdn.imgurl.ir/uploads/i82081_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJEIIDAEAGCDABG"
+        },
+
+        {
+            id: 16,
+            title: "کانتر۲ مود منو فارسی بدون دیتا",
+            image: "https://cdn.imgurl.ir/uploads/q320766_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJEJHGAIFFEEABG"
+        },
+
+        {
+            id: 17,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/n84727_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJHHJIFCAFFDABG"
+        },
+
+        {
+            id: 18,
+            title: "کانتر۲ مود شده بدون منو",
+            image: "https://cdn.imgurl.ir/uploads/l460626_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJHIIAFGDAIJABG"
+        },
+
+        {
+            id: 19,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/z19771_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BGJHJBCFIDAHDABG"
+        },
+
+        {
+            id: 20,
+            title: "کانتر۲ مود منو عالی",
+            image: "https://cdn.imgurl.ir/uploads/v898901_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BHAAHJDADEDFJABG"
+        },
+
+        {
+            id: 21,
+            title: "کانتر۲ مود منو",
+            image: "https://cdn.imgurl.ir/uploads/p742_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BHJJIEDHAJFEIABG"
+        },
+
+        {
+            id: 22,
+            title: "کانتر۲ مود منو جدید خفن",
+            image: "https://cdn.imgurl.ir/uploads/o448790_IMG_20260808_025040_118.jpg",
+            url: "https://rubika.ir/Counter2Mod/BIAAFFHAFGHEFABG"
+        }
+
+    ],
+
+    apps: [
+
+        {
+            id: 1,
+            title: "سورس ساز حرفه‌ای",
+            image:
+                "https://cdn.imgurl.ir/uploads/j13307_InShot_20260822_153149438.png",
+            url:
+                "https://cdn.imgurl.ir/uploads/b590319___.zip"
+        },
+
+        {
+            id: 2,
+            title: "کد ساز سایت",
+            image:
+                "https://cdn.imgurl.ir/uploads/v069309_InShot_20260907_003520819.png",
+            url:
+                "https://cdn.imgurl.ir/uploads/v73960___.zip"
+        }
+
+    ]
+
+};
